a9c264ae72b024244622a9a1ef6da3652ebb0466
zevdvlpr <zevdvlpr@gmail.com>
2021-05-09 18:20:44 -0300

    ✨ feat: login page

---
+++ src/pages/index.tsx
@@ -0,0 +1,74 @@
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { Input } from '../components/Form/Input';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
+export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async data => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(data);
+  };
+
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        onSubmit={handleSubmit(handleSignIn)}
+        width="100%"
+        maxWidth="360px"
+        flexDir="column"
+        bg="gray.800"
+        p="8"
+        borderRadius="8px"
+      >
+        <Stack spacing="4">
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            error={errors.email}
+            {...register('email')}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Password"
+            error={errors.password}
+            {...register('password')}
+          />
+        </Stack>
+
+        <Button
+          type="submit"
+          isLoading={isSubmitting}
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
