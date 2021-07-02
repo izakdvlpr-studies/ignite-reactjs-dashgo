51c0122cdeaf9eae7f8f88e9bca52f7ece58f8ea
zevdvlpr <zevdvlpr@gmail.com>
2021-07-02 14:47:49 -0300

    feat: useUsers hook

---
+++ src/hooks/useUsers.tsx
@@ -0,0 +1,49 @@
+import { useQuery, UseQueryOptions, UseQueryResult } from 'react-query';
+
+import { api } from '../services/api';
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+}
+interface GetUsersResponse {
+  users: User[];
+  totalCount: number;
+}
+
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get('users', {
+    params: {
+      page,
+    },
+  });
+
+  const totalCount = Number(headers['x-total-count']);
+
+  const users = data.users.map(user => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.created_at).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: '2-digit',
+      }),
+    };
+  });
+
+  return {
+    users,
+    totalCount,
+  };
+}
+
+export function useUsers(page: number, options?: UseQueryOptions) {
+  return useQuery(['users', page], () => getUsers(page), {
+    staleTime: 1000 * 60 * 10, // 10 minutos
+    ...options,
+  }) as UseQueryResult<GetUsersResponse, unknown>;
+}
