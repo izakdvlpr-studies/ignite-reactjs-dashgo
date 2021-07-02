2932a5b728d5859061716efbd511315f38568596
zevdvlpr <zevdvlpr@gmail.com>
2021-07-02 14:47:26 -0300

    feat: react query client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,30 @@
 import { AppProps } from 'next/app';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { SideBarDrawerProvider } from '../contexts/SideBarDrawerContext';
 
+import { queryClient } from '../services/queryClient';
+
+import { makeServer } from '../services/mirageJS';
+
 import { theme } from '../styles/theme';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SideBarDrawerProvider>
-        <Component {...pageProps} />
-      </SideBarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SideBarDrawerProvider>
+          <Component {...pageProps} />
+        </SideBarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
